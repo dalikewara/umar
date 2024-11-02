2d64caa98423370273399179ff1aa0662f5f35ec
Dali Kewara <dalikewara@gmail.com>
2024-11-02 16:20:09 +0700

    update some scripts

--- umar.sh
+++ umar.sh
@@ -2,7 +2,7 @@
 
 # LAST COUNTER FOR FUNCTION VARIABLE = 32
 
-version="v2.6.2"
+version="v2.6.3"
 pid=$$
 distro=""
 de=""
@@ -323,7 +323,7 @@ command_setupfresharch() {
   install_package "git" "vim" "curl" "htop" "neofetch" "bash" "zsh" "chromium" "make" "xorg-xrandr" "libinput" "xf86-input-libinput" \
   "xorg-server" "xorg-xinput" "polkit" "pulsemixer" "xfce4-terminal" "iwd" "amd-ucode" "intel-ucode" "lm_sensors" "bc" "base-devel" \
   "linux-lts-headers" "pipewire" "pipewire-audio" "pipewire-pulse" "wget" "xsensors" "unzip" "sof-firmware" "alsa-firmware" "pipewire-alsa" \
-  "pipewire-jack" "wireplumber" "lib32-pipewire" "pavucontrol" "lib32-pipewire-jack" "alsa-card-profiles"
+  "pipewire-jack" "wireplumber" "pavucontrol" "alsa-card-profiles"
 
   if ! is_file_exist "$ssh_keygen_filepath"; then
     ssh-keygen -t ed25519
