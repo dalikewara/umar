e31c4af45d66fb05a070cc7b8ca9f7e015baf65b
Dali Kewara <dalikewara@gmail.com>
2024-11-14 19:01:02 +0700

    update some scripts

--- umar.sh
+++ umar.sh
@@ -2,7 +2,7 @@
 
 # LAST COUNTER FOR FUNCTION VARIABLE = 34
 
-version="v2.8.10"
+version="v2.8.11"
 pid=$$
 distro=""
 de=""
@@ -595,9 +595,10 @@ fi
   sed -i 's/bindsym \$mod+Return exec i3\-sensible\-terminal/\#bindsym \$mod+Return exec i3\-sensible\-terminal/g' "$config_i3wm_filepath"
   # shellcheck disable=SC2016
   sed -i 's/bindsym \$mod+space focus mode\_toggle/\#bindsym \$mod+space focus mode\_toggle/g' "$config_i3wm_filepath"
+  sed -i 's/bindsym \$mod+Shift+space floating toggle/\#bindsym \$mod+Shift+space floating toggle/g' "$config_i3wm_filepath"
 
-  if ! grep -qF "bindsym \$mod+Return exec xfce4-terminal" "$config_i3wm_filepath"; then
-    echo "bindsym \$mod+Return exec xfce4-terminal" >> "$config_i3wm_filepath"
+  if ! grep -qF "bindsym \$mod+Return exec --no-startup-id xfce4-terminal" "$config_i3wm_filepath"; then
+    echo "bindsym \$mod+Return exec --no-startup-id xfce4-terminal" >> "$config_i3wm_filepath"
   fi
 
   if ! grep -qF "bindsym \$mod+space exec umar window" "$config_i3wm_filepath"; then
